f20e01a307cb9196476e7c65d505e2707eccd806
Nicholas Koblitz <nicholask1@outlook.com>
2021-11-26 10:24:35 -0500

    added styles and finished main files

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("Let's get this party started!");
+const $form = $("#search-form");
+const $input = $("#search");
+const token = "UMtzstOrkcp6yXBX2KY6O81EJ392YPiG"
+
+
+function appendGif(data) {
+
+    const gifArr = data.data
+    const randomNum = Math.floor(Math.random() * gifArr.length + 1)
+    const gifUrl = gifArr[randomNum].images.original.url
+    
+    $("#gif-area").append(`<img src="${gifUrl}" class="size">`)
+
+}
+
+
+$form.on('submit', async function(e) {
+    e.preventDefault();
+    const search = $input.val();
+    const res = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {q: search, api_key: token}});
+
+    appendGif(res.data);
+    $input.val('');
+
+})
+
+$("#delete-gifs").on('click', function() {
+    $("#gif-area").empty();
+})
